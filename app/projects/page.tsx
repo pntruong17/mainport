2a5aaeba90a8d5cae3620b94b2d1f9fb56186602
pntruong17 <pntruong17@gmail.com>
2023-06-01 13:41:49 +0700

    del content

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
